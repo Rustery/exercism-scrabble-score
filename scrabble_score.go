9624cfe304d2dd63b1457cf2f21f9ec50671600b
Rustam <rustery@gmail.com>
2020-02-19 12:32:51 +0300

    first commit

---
+++ scrabble_score.go
@@ -0,0 +1,42 @@
+// Package scrabble is Exercism.io exercise
+package scrabble
+
+import "unicode"
+
+var values = map[rune]int{
+	'A': 1,
+	'E': 1,
+	'I': 1,
+	'O': 1,
+	'U': 1,
+	'L': 1,
+	'N': 1,
+	'R': 1,
+	'S': 1,
+	'T': 1,
+	'D': 2,
+	'G': 2,
+	'B': 3,
+	'C': 3,
+	'M': 3,
+	'P': 3,
+	'F': 4,
+	'H': 4,
+	'V': 4,
+	'W': 4,
+	'Y': 4,
+	'K': 5,
+	'J': 8,
+	'X': 8,
+	'Q': 10,
+	'Z': 10,
+}
+
+// Score — Calculating Scrabble score for the word
+func Score(input string) int {
+	var score int
+	for _, v := range input {
+		score += values[unicode.ToUpper(v)]
+	}
+	return score
+}
